c47e6e968aac75a2644b57c059355608b0fcd2e0
RafaGiaretta <rafaelgiaretta@ymail.com>
2024-06-24 12:01:06 -0300

    Images

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ function pararAudio() {
     iniciar.pause();
     iniciar.currentTime = 0; // Reinicia o áudio para o início
 }
+
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 10,    
+    slidesPerView:3,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    }
+});
